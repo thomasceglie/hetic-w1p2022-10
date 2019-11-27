54a051fb31bf786aed249a08fc8d256540f40a8c
Hgo0123 <hcordillot@gmail.com>
2019-11-27 17:25:01 +0100

    modified animation product - ok, nav fixed - ok, font helvetica, add icon

--- script.js
+++ script.js
@@ -1,65 +1,91 @@
-var ids = ["p1", "p2", "p3", "p4", "p5"];
+// var ids = ["p1", "p2", "p3", "p4", "p5"];
 
-ids.forEach(function(id) {
-  var element = document.getElementById(id);
+// ids.forEach(function(id) {
+//   var element = document.getElementById(id);
 
-  var waypoint = new Waypoint({
-    element: element,
-    handler: function(direction) {
-      element.classList.add("is-reached75");
-    },
-    offset: "75%"
-  });
-});
+//   var waypoint = new Waypoint({
+//     element: element,
+//     handler: function(direction) {
+//       element.classList.add("is-reached75");
+//     },
+//     offset: "75%"
+//   });
+// });
 
-var ids = ["p1", "p2", "p3", "p4", "p5"];
+// var ids = ["p1", "p2", "p3", "p4", "p5"];
 
-ids.forEach(function(id) {
-  var element = document.getElementById(id);
+// ids.forEach(function(id) {
+//   var element = document.getElementById(id);
 
-  var waypoint = new Waypoint({
-    element: element,
-    handler: function(direction) {
-      element.className = "is-reached50";
-    },
-    offset: "50%"
-  });
-});
+//   var waypoint = new Waypoint({
+//     element: element,
+//     handler: function(direction) {
+//       element.className = "is-reached50";
+//     },
+//     offset: "50%"
+//   });
+// });
 
-var ids = ["p1", "p2", "p3", "p4", "p5"];
+// var ids = ["p1", "p2", "p3", "p4", "p5"];
 
-ids.forEach(function(id) {
-  var element = document.getElementById(id);
+// ids.forEach(function(id) {
+//   var element = document.getElementById(id);
 
-  var waypoint = new Waypoint({
-    element: element,
-    handler: function(direction) {
-      element.className = "is-reached25";
-    },
-    offset: "25%"
-  });
-});
+//   var waypoint = new Waypoint({
+//     element: element,
+//     handler: function(direction) {
+//       element.className = "is-reached25";
+//     },
+//     offset: "25%"
+//   });
+// });
 
-var ids = ["p1", "p2", "p3", "p4", "p5"];
+// var ids = ["p1", "p2", "p3", "p4", "p5"];
 
-ids.forEach(function(id) {
-  var element = document.getElementById(id);
+// ids.forEach(function(id) {
+//   var element = document.getElementById(id);
 
-  var waypoint = new Waypoint({
-    element: element,
-    handler: function(direction) {
-      element.className = "is-reached5";
-    },
-    offset: "5%"
-  });
-});
+//   var waypoint = new Waypoint({
+//     element: element,
+//     handler: function(direction) {
+//       element.className = "is-reached5";
+//     },
+//     offset: "5%"
+//   });
+// });
 
 var element = document.querySelector(".slogan");
 
 var waypoint = new Waypoint({
   element: element,
   handler: function(direction) {
-    element.classList.add("fadeInUp");
+    element.classList.add("is-fadeInUp");
   },
-  offset: "70%"
+  offset: "90%"
+});
+
+var ids = ["p1", "p2", "p3", "p4", "p5"];
+
+ids.forEach(function(id) {
+  var element = document.getElementById(id);
+
+  var waypoint = new Waypoint({
+    element: element,
+    handler: function(direction) {
+      element.classList.add("is-reached60");
+    },
+    offset: "40%"
+  });
+});
+
+ids.forEach(function(id) {
+  var element = document.getElementById(id);
+
+  var waypoint = new Waypoint({
+    element: element,
+    handler: function(direction) {
+      element.classList.add("is-fixed");
+    },
+    offset: "80px"
+  });
 });
